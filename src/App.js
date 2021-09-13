c89c3c770284dc52511b6c71fcc93963bd36fc9a
Range Quest <range.quest@gmail.com>
2021-09-13 19:51:32 +0530

    Movie table is now ready. Can remove movies.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
+import Movies from "./components/movies";
 
 function App() {
   return (
     <main role="main" className="container">
-      <h1>Hello Movies</h1>
+      <Movies />
     </main>
   );
 }
---
+++ src/components/movies.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+class Movies extends Component {
+  state = {
+    movies: getMovies(),
+  };
+
+  handleDelete = movie => {
+    let movies = this.state.movies.filter(m => m._id !== movie._id);
+    this.setState({ movies });
+  };
+
+  render() {
+    const { length: count } = this.state.movies;
+    if (count === 0) return <p>No movies in DB.</p>;
+
+    return (
+      <React.Fragment>
+        <p>Showing {count} movies in the database</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Genre</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Rate</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map(movie => {
+              return (
+                <tr key={movie._id}>
+                  <td>{movie.title}</td>
+                  <td>{movie.genre.name}</td>
+                  <td>{movie.numberInStock}</td>
+                  <td>{movie.dailyRentalRate}</td>
+                  <td>
+                    <button onClick={() => this.handleDelete(movie)} type="button" className="btn btn-danger btn-sm">
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Movies;
