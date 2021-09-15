aac9c965ad85f89314ab1f82d8aa0ef97af40099
Range Quest <range.quest@gmail.com>
2021-09-15 18:41:14 +0530

    Using Refs. Minimize usage.

---
+++ src/components/common/loginForm.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+class LoginForm extends React.Component {
+  username = React.createRef();
+  componentDidMount = () => {
+    this.username.current.focus();
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    const username = this.username.current.value;
+    console.log("Submitted", username);
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input ref={this.username} id="username" type="text" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input id="password" type="text" className="form-control" />
+          </div>
+          <button className="btn btn-primary">Login</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
