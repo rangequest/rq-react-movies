1cda91895e166efa1bd065c2ce0c8986344d0417
Range Quest <range.quest@gmail.com>
2021-09-15 17:09:05 +0530

    Navbar with pages implemented.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import Navigation from "./components/navigation";
 import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
 
 function App() {
   return (
-    <main role="main" className="container">
-      <Movies />
-    </main>
+    <BrowserRouter>
+      <Navigation />
+      <main role="main" className="container">
+        <Switch>
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect from="/" to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/customers.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+class Customers extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Customers</h1>
+      </div>
+    );
+  }
+}
+
+export default Customers;
