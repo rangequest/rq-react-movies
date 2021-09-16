002ec83fbad558b5e24b71320c7ea54023122844
Range Quest <range.quest@gmail.com>
2021-09-16 07:13:48 +0530

    Property validation with Joi.

--- src/components/common/loginForm.jsx
+++ src/components/common/loginForm.jsx
@@ -34,14 +34,11 @@ class LoginForm extends React.Component {
   };
 
   validateProperty = ({ name, value }) => {
-    if (name === "username") {
-      if (value.trim() === "") return "Username is required";
-      // ...
-    }
-    if (name === "password") {
-      if (value.trim() === "") return "Password is required";
-      // ...
-    }
+    const obj = { [name]: value };
+    const schema = { [name]: this.schema[name] };
+    const { error } = Joi.validate(obj, schema);
+    if (!error) return null;
+    return error ? error.details[0].message : null;
   };
 
   handleChange = ({ currentTarget: input }) => {
