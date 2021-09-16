9e70962ebf85c6dc1a9a4b0589c8ccecaa38ad66
Range Quest <range.quest@gmail.com>
2021-09-16 22:51:17 +0530

    Users can now login and get json web tokens.

---
+++ src/services/authService.js
@@ -0,0 +1,8 @@
+import http from './httpService'
+import { apiUrl } from '../config.json'
+
+const apiEndpoint = apiUrl + '/auth'
+
+export const login = (email, password) => {
+  return http.post(apiEndpoint, { email, password })
+}
