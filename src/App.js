e2300d67f179f8cace47bbdba413befd1a55f27e
Range Quest <range.quest@gmail.com>
2021-09-16 23:24:33 +0530

    Recognize the current user.

--- src/App.js
+++ src/App.js
@@ -10,27 +10,40 @@ import LoginForm from './components/loginForm'
 import RegisterForm from './components/registerForm'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import jwtDeocode from 'jwt-decode'
 
-function App() {
-  return (
-    <React.Fragment>
-      <ToastContainer />
-      <NavBar />
-      <main role="main" className="container">
-        <Switch>
-          <Route path="/movies/:id" component={MovieForm} />
-          <Route path="/login" component={LoginForm} />
-          <Route path="/register" component={RegisterForm} />
-          <Route path="/movies" component={Movies} />
-          <Route path="/customers" component={Customers} />
-          <Route path="/rentals" component={Rentals} />
-          <Route path="/not-found" component={NotFound} />
-          <Redirect exact from="/" to="/movies" />
-          <Redirect to="/not-found" />
-        </Switch>
-      </main>
-    </React.Fragment>
-  )
+class App extends React.Component {
+  state = {}
+
+  componentDidMount() {
+    try {
+      const jwt = localStorage.getItem('token')
+      const user = jwtDeocode(jwt)
+      this.setState({ user })
+    } catch (error) {}
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <ToastContainer />
+        <NavBar user={this.state.user} />
+        <main role="main" className="container">
+          <Switch>
+            <Route path="/movies/:id" component={MovieForm} />
+            <Route path="/login" component={LoginForm} />
+            <Route path="/register" component={RegisterForm} />
+            <Route path="/movies" component={Movies} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect exact from="/" to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
+    )
+  }
 }
 
 export default App
