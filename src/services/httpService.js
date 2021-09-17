296b0aebe51c61629df66e588f9117864880baaf
Range Quest <range.quest@gmail.com>
2021-09-17 08:01:22 +0530

    Set x-auth-header in every request.

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,6 +1,9 @@
 import axios from 'axios'
 import { toast } from 'react-toastify'
+import auth from './authService'
 import logger from './logService'
+
+axios.defaults.headers.common['x-auth-token'] = auth.getJwt()
 
 axios.interceptors.response.use(null, error => {
   const expectedError =
