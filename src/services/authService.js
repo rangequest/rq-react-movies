a030a110844563839fbacd534dbb07a409e6c141
Range Quest <range.quest@gmail.com>
2021-09-17 08:01:38 +0530

    Auth service revamped.

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,8 +1,42 @@
 import http from './httpService'
 import { apiUrl } from '../config.json'
+import jwtDeocode from 'jwt-decode'
 
 const apiEndpoint = apiUrl + '/auth'
+const tokenKey = 'rq-token'
 
-export const login = (email, password) => {
-  return http.post(apiEndpoint, { email, password })
+export const login = async (email, password) => {
+  const { data: jwt } = await http.post(apiEndpoint, { email, password })
+  localStorage.setItem(tokenKey, jwt)
 }
+
+export const loginWithJwt = jwt => {
+  localStorage.setItem(tokenKey, jwt)
+}
+
+export const logout = () => {
+  localStorage.removeItem(tokenKey)
+}
+
+export const getJwt = () => {
+  return localStorage.getItem(tokenKey)
+}
+
+export const getCurrentUser = () => {
+  try {
+    const jwt = localStorage.getItem(tokenKey)
+    return jwtDeocode(jwt)
+  } catch (error) {
+    return null
+  }
+}
+
+const auth = {
+  login,
+  logout,
+  getCurrentUser,
+  loginWithJwt,
+  getJwt,
+}
+
+export default auth
